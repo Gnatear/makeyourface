550062b80ddde18ade69add47e351aef597613c9
raetang <zh433931@dal.ca>
2024-01-28 10:51:22 -0400

    speed

--- Make Your Face/Assets/MoveTarget.cs
+++ Make Your Face/Assets/MoveTarget.cs
@@ -5,8 +5,8 @@ public class MoveTarget : MonoBehaviour
     public float initialSpeed;
     public Transform wallLeft;
     public Transform wallRight;
-    public Transform wallFront;
-    public Transform wallBack;
+    public Transform wallTop;
+    public Transform wallBottom;
 
     void Start()
     {
@@ -24,13 +24,25 @@ public class MoveTarget : MonoBehaviour
         Vector3 newPosition = transform.position + randomDirection * initialSpeed * Time.deltaTime;
 
         newPosition.x = Mathf.Clamp(newPosition.x, wallLeft.position.x, wallRight.position.x);
-        newPosition.z = Mathf.Clamp(newPosition.z, wallFront.position.z, wallBack.position.z);
+        newPosition.z = Mathf.Clamp(newPosition.z, wallTop.position.z, wallBottom.position.z);
 
         transform.position = newPosition;
     }
 
     public void ResetTarget()
     {
+        initialSpeed = Random.Range(5f, 10f); 
+        int counter == 1;
+        if(ResetTarget()){
+
+            for(counter = 1; counter < 10000000; counter++){
+
+                initialSpeed = Random.Range(5f * counter, 10f * counter); 
+
+            }
+
+        }
         transform.position = new Vector3(Random.Range(wallLeft.position.x, wallRight.position.x), transform.position.y, Random.Range(wallFront.position.z, wallBack.position.z));
+    }
     }
 }
