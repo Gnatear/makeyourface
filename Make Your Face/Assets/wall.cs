4e93f5a650f0811a2b2894f3abec2155c4940ec9
xaingling <neolx199@gmail.com>
2024-01-28 07:04:18 -0400

    improve the walls, seperate people from amin scense

--- Make Your Face/Assets/wall.cs
+++ Make Your Face/Assets/wall.cs
@@ -1,41 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class wall : MonoBehaviour
 {
     public GameObject wallStickerPrefab;
-    public Vector3 wallScale;
+    [FormerlySerializedAs("wallScale")] public Vector3 wallPosition;
 
 
     void Start()
     {
-        wallScale = transform.localScale;
+        wallPosition = transform.localPosition;
     }
 
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log(gameObject.transform.position);
         Vector3 bulletPos = other.transform.position;
-        Debug.Log("hit pos: " + bulletPos);
-        if (wallScale.z == 1)
-        {
-            Vector3 stickerRotation = new Vector3(90, 0, 0);
-            createSticker(bulletPos, transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        }
+        createSticker(bulletPos, transform.localPosition);
+        
     }
 
-    private void createSticker(Vector3 stickerPosition, float x, float y, float z)
+    private void createSticker(Vector3 stickerPosition, Vector3 wallPosition)
     {
         float stickerRotationX = 0;
         float stickerRotationY = 0;
         float stickerRotationZ = 0;
         
-        if (z == 1)
+        if (wallPosition.y == 0)
+        {
+            stickerRotationX = 0;
+        }
+        else
         {
             stickerRotationX = 90;
+            if (this.wallPosition.x > 0)
+            {
+                stickerRotationY = -90;
+            }
+            if (this.wallPosition.x < 0)
+            {
+                stickerRotationY = 90;
+            }
+            if (this.wallPosition.z < 0)
+            {
+                stickerRotationY = 0;
+            }
+            if (this.wallPosition.z > 0)
+            {
+                stickerRotationY = 180;
+            }
         }
-        
         var sticker = Instantiate(wallStickerPrefab, stickerPosition, Quaternion.Euler(stickerRotationX, stickerRotationY, stickerRotationZ));
     }
 }
