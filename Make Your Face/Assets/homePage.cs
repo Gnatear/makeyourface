1708291b44f2a588d3f27b06c204dd0766194732
xaingling <neolx199@gmail.com>
2024-01-27 21:15:54 -0400

    rebase and resolve conflict

--- Make Your Face/Assets/homePage.cs
+++ Make Your Face/Assets/homePage.cs
@@ -9,13 +9,7 @@ public class homePage : MonoBehaviour
     public void PlayGame(){
         
         SceneManager.LoadSceneAsync(1);
-
-    }
-
-    public void QuitGame(){
-
-        Application.Quit();
-
+        
     }
 
     public void QuitGame(){
