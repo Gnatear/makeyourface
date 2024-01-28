74b4856be4576f5ebc1db30db44689d4d4364cbd
raetang <zh433931@dal.ca>
2024-01-28 10:45:48 -0400

    restart

--- Make Your Face/Assets/MoveTarget.cs
+++ Make Your Face/Assets/MoveTarget.cs
@@ -1,13 +1,17 @@
-using System.Collections;
 using UnityEngine;
 
 public class MoveTarget : MonoBehaviour
 {
-    public float speed;
-    public Transform wall_left;
-    public Transform wall_right;
-    public Transform wall_front;
-    public Transform wall_back;
+    public float initialSpeed;
+    public Transform wallLeft;
+    public Transform wallRight;
+    public Transform wallFront;
+    public Transform wallBack;
+
+    void Start()
+    {
+        ResetTarget();
+    }
 
     void Update()
     {
@@ -16,16 +20,17 @@ public class MoveTarget : MonoBehaviour
 
     void MoveRandomly()
     {
-    
         Vector3 randomDirection = new Vector3(Random.Range(-1f, 1f), 0f, Random.Range(-1f, 1f)).normalized;
-        
-        Vector3 newPosition = transform.position + randomDirection * speed * Time.deltaTime;
+        Vector3 newPosition = transform.position + randomDirection * initialSpeed * Time.deltaTime;
 
-        newPosition.x = Mathf.Clamp(newPosition.x, wall_left.position.x, wall_right.position.x);
-        newPosition.z = Mathf.Clamp(newPosition.z, wall_front.position.z, wall_back.position.z);
+        newPosition.x = Mathf.Clamp(newPosition.x, wallLeft.position.x, wallRight.position.x);
+        newPosition.z = Mathf.Clamp(newPosition.z, wallFront.position.z, wallBack.position.z);
 
-
-        // Set the new position
         transform.position = newPosition;
+    }
+
+    public void ResetTarget()
+    {
+        transform.position = new Vector3(Random.Range(wallLeft.position.x, wallRight.position.x), transform.position.y, Random.Range(wallFront.position.z, wallBack.position.z));
     }
 }
